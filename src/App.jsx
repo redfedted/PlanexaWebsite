b5ca8864227606a223875b3a32ae1b47a972dbaa
Shreyas M S <shreyasmadappady20@gmail.com>
2024-08-29 22:32:52 +0530

    Added Delete Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
 import InstallApp from "./pages/InstallApp.jsx";
 import Home from "./pages/Home.jsx";
+import Delete from "./pages/DeleteProfile.jsx";
 
 function App() {
   return (
@@ -8,11 +9,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home> </Home>} />
         <Route path="/downloads/*" element={<InstallApp />} />
+        <Route path="/deleteprofile/*" element={<Delete />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-
 
 export default App;
